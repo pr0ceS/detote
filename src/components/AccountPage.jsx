e811e7b28945d8e94d5380a9f383ed638e14bf74
MKB <xxmkbozzzxx@gmail.com>
2023-12-08 16:50:04 +0100

    react-icons remove

--- src/components/AccountPage.jsx
+++ src/components/AccountPage.jsx
@@ -3,7 +3,6 @@ import Cookies from "js-cookie";
 import { DecodeCookie } from "./DecodeCookie";
 import { urlString } from "../utils/api";
 import Logout from "./Logout";
-import { FiLogOut } from "react-icons/fi";
 import Price from "./Price";
 import moment from 'moment';
 import FadeIn from "react-fade-in";
@@ -72,7 +71,7 @@ const AccountPage = () => {
 					<div className="greeting">
 						<h1>Welcome back, <span className={`fade-in ${userData && userData.name && "show"}`}>{userData && userData.name && userData.name.includes(' ') ? userData.name.split(' ')[0] : userData.name}</span></h1>
 						<p>Use your account to manage your orders, track deliveries and access your order history.</p>
-						<button className="button" onClick={() => Logout()}><FiLogOut />Sign out</button>
+						<button className="button" onClick={() => Logout()}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>Sign out</button>
 					</div>
 					<div className="orders">
 						<h2>My Orders</h2>
